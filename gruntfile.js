d183ab41c34d494cadc569f079b5524a36e92f5c
Marco Kunz <marco.kunz1982@gmail.com>
2014-04-11 15:48:23 +0200

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,5 @@
+/*
+  This is the Gruntfile you are going to fill with wonderful Javascript.
+  I don't know much about JS (apart from this shortcut) but this is such an
+  easy task, that even I got my head around this.
+*/
